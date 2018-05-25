548d1a82ecb98780ec7e54fa6a1a0d85cbf6495d
Your Name <you@gmail.com>
2018-05-25 17:24:07 -0400

    controller

--- src/main/java/com/example/PublishingService/PublishingServiceApplication.java
+++ src/main/java/com/example/PublishingService/PublishingServiceApplication.java
@@ -1,5 +1,8 @@
 package com.example.PublishingService;
 
+import com.example.PublishingService.repositories.BikeRepository;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +11,9 @@ public class PublishingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PublishingServiceApplication.class, args);
+
+
+
+
 	}
 }
--- src/main/java/com/example/PublishingService/controllers/BikesController.java
+++ src/main/java/com/example/PublishingService/controllers/BikesController.java
@@ -1,40 +1,35 @@
 package com.example.PublishingService.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import com.example.PublishingService.models.Bike;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import com.example.PublishingService.repositories.BikeRepository;
+//import com.example.PublishingService.repositories.BikeRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/v1/bikes")
 public class BikesController {
-
-    @RestController
-    @RequestMapping("/api/v1/bikes")
-    public  class BikeController{
-
-        @Autowired
-        private BikeRepository bikeRepository;
 
         @GetMapping
         public  List<Bike> list(){
-
-            return bikeRepository.findAll();
+            List<Bike> bikes = new ArrayList<>();
+            return bikes;
         }
 
         @PostMapping
         @ResponseStatus(HttpStatus.OK)
         public void create(@RequestBody Bike bike){
-            bikeRepository.save(bike);
+
         }
 
         @GetMapping("/{id}")
          public Bike  get(@PathVariable("id") long id) {
 
-           return  bikeRepository.getOne(id);
+            return  new Bike();
 
         }
     }
-}
+
