34ec876c247524dcff75d2498783968ff8bee6b0
Your Name <you@gmail.com>
2018-05-26 09:36:29 -0400

    done with java part for now

--- src/main/java/com/example/PublishingService/controllers/BikesController.java
+++ src/main/java/com/example/PublishingService/controllers/BikesController.java
@@ -2,6 +2,8 @@ package com.example.PublishingService.controllers;
 
 //import org.springframework.beans.factory.annotation.Autowired;
 import com.example.PublishingService.models.Bike;
+import com.example.PublishingService.repositories.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 //import com.example.PublishingService.repositories.BikeRepository;
@@ -13,22 +15,24 @@ import java.util.List;
 @RequestMapping("/api/v1/bikes")
 public class BikesController {
 
+    @Autowired
+    private BikeRepository bikeRepository;
         @GetMapping
         public  List<Bike> list(){
-            List<Bike> bikes = new ArrayList<>();
-            return bikes;
+          //  List<Bike> bikes = new ArrayList<>();
+            return bikeRepository.findAll();
         }
 
         @PostMapping
         @ResponseStatus(HttpStatus.OK)
         public void create(@RequestBody Bike bike){
-
+            bikeRepository.save(bike);
         }
 
         @GetMapping("/{id}")
          public Bike  get(@PathVariable("id") long id) {
 
-            return  new Bike();
+           return bikeRepository.getOne(id);
 
         }
     }
